d8d1fe92bde62982c82ef4da75957884abafe98b
Kaustubh Kumar <kaustubhkumar1998@gmail.com>
2019-07-23 23:02:20 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,23 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">Distirct Collector Management System</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>The tasks of a district collector are becoming more tedious becauseof the growing number of people applying for everything to thecollectorate office. The collector is the highest responsible officerwho has to take care of everything in his district. As such, asoftware to assist his duties and automates most of the tasks of collectorate is highly appreciated. District collectorate managementsystem is one such website which helps to improve the productivityof district collector’s office people but also it helps the citizens tounderstand the status of their jobs pending the office.
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
